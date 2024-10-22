1b672f4b55709f3e12631151654f80fe674226c7
Rishabh5911 <rishabhpokhriyal259@gmail.com>
2024-10-22 13:13:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { AppContext } from "./context/contextApi";
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header';
+import Feed from './components/Feed';
+import SearchResult from './components/SearchResult';
+import VideoDetails from './components/VideoDetails';
+import { Route,Routes } from "react-router-dom";
+
+
+
+function App() {
+  return (
+   
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header/>
+          <Routes>
+            <Route path='/' exact element={<Feed/>}/>
+            <Route path='/searchResult/:searchQuery' element={<SearchResult/>}/>
+            <Route path='/video/:id' element={<VideoDetails/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
+  );
+}
+
+export default App;
